bd7b33cf10351daf13f81b4128bdaed479839751
Jussi Kurki <jussi.kurki@iki.fi>
2014-05-27 12:48:29 +0300

    testing null action

--- src/test/java/fi/thl/example/servlet/SimpleServletTest.java
+++ src/test/java/fi/thl/example/servlet/SimpleServletTest.java
@@ -1,4 +1,30 @@
 package fi.thl.example.servlet;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
+
 public class SimpleServletTest {
+
+    @Test(expected = NullPointerException.class)
+    public void nullActionShouldThrowNullPointerException()
+            throws ServletException, IOException {
+        SimpleServlet simpleServlet = new SimpleServlet();
+
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        given(req.getParameter("action")).willReturn(null);
+
+        HttpServletResponse res = mock(HttpServletResponse.class);
+
+        // throws npe
+        simpleServlet.doGet(req, res);
+    }
+
 }
