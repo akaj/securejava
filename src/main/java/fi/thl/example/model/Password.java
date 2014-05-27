67756fae6e1dcc4a534e7f10e176e832ef6975cb
Jussi Kurki <jussi.kurki@iki.fi>
2014-05-27 13:50:59 +0300

    remove imports

--- src/main/java/fi/thl/example/model/Password.java
+++ src/main/java/fi/thl/example/model/Password.java
@@ -1,11 +1,9 @@
 package fi.thl.example.model;
 
-import org.mockito.internal.matchers.Equals;
-
 public class Password extends SecureString {
-	protected final static String pattern = ".*";
-	
-	public Password(String s) {
-		super(s, pattern);
-	}
+    protected final static String pattern = ".*";
+
+    public Password(String s) {
+        super(s, pattern);
+    }
 }
